85875775c6326453e212c15258d6a199924af250
Jackson Kettel <69734811+Kettelcorn@users.noreply.github.com>
2023-02-03 16:40:45 -0800

    updated to include which shows I have watched

--- src/main/java/Listeners/MyListeners.java
+++ src/main/java/Listeners/MyListeners.java
@@ -8,6 +8,7 @@ import java.util.List;
 import dev.katsute.mal4j.MyAnimeList;
 import dev.katsute.mal4j.anime.Anime;
 import dev.katsute.mal4j.anime.AnimeListStatus;
+import dev.katsute.mal4j.query.UserAnimeListQuery;
 import dev.katsute.mal4j.query.UserMangaListQuery;
 import dev.katsute.mal4j.user.User;
 import dev.katsute.mal4j.user.UserAnimeStatistics;
@@ -26,18 +27,25 @@ import org.jetbrains.annotations.NotNull;
 public class MyListeners extends ListenerAdapter {
 
     private List<Anime> search;
+    private List<Anime> shows;
+    private MyAnimeList mal;
+    private List<AnimeListStatus> animeListStatus;
+
+
 
     public MyListeners(){
         search = new ArrayList<>();
+        animeListStatus = new ArrayList<>();
     }
     @Override
     public void onSlashCommandInteraction(@NotNull SlashCommandInteractionEvent event) {
         if (event.getName().equals("mal-bot")) {
-
+            mal = MyAnimeList.withClientID("ed63f8418f1cdf0c626aae8618705f15");
             TextInput show = TextInput.create("Search", "Search", TextInputStyle.SHORT)
                     .setPlaceholder("Enter show here").build();
             Modal modal = Modal.create("modmail", "MyAnimeListBot").addActionRow(show).build();
             event.replyModal(modal).queue();
+            animeListStatus = mal.getUserAnimeListing("Kettelcorn").withStatus("completed").withLimit(100).search();
         }
     }
 
@@ -45,8 +53,9 @@ public class MyListeners extends ListenerAdapter {
     public void onModalInteraction(@NotNull ModalInteractionEvent event) {
         if (event.getModalId().equals("modmail")) {
             String show = event.getValue("Search").getAsString();
-            MyAnimeList mal = MyAnimeList.withClientID("ed63f8418f1cdf0c626aae8618705f15");
             search = mal.getAnime().withQuery(show).search();
+
+
             StringSelectMenu.Builder builder = StringSelectMenu.create("select-anime");
             for (Anime anime: search) {
                 builder.addOption(anime.getTitle(), anime.getTitle());
@@ -63,13 +72,21 @@ public class MyListeners extends ListenerAdapter {
                     selectedShow = anime;
                 }
             }
-            String imageURL = selectedShow.getMainPicture().getLargeURL();
+            String hasWatched = "not";
+            for (AnimeListStatus listStatus : animeListStatus) {
+                if (listStatus.getAnime().getID().equals(selectedShow.getID())) {
+                    hasWatched = "";
+                }
+            }
+
+            int id = selectedShow.getID().intValue();
+            String url = "https://myanimelist.net/anime/" + id;
             EmbedBuilder embedBuilder = new EmbedBuilder();
             embedBuilder.setTitle(selectedShow.getTitle());
             embedBuilder.setColor(Color.CYAN);
-            embedBuilder.setImage(imageURL);
-            embedBuilder.addField("show description", "Your show is " + selectedShow.getTitle() + "\n" +
-                    "Its ranking is " + selectedShow.getRank(), false);
+            embedBuilder.setImage(selectedShow.getMainPicture().getLargeURL());
+            embedBuilder.addField(url, "Episodes: " + selectedShow.getEpisodes() + "\n" +
+                      "Kettelcorn has " + hasWatched + " watched this show", false);
             embedBuilder.setFooter("Request made by " + event.getMember().getUser().getName(),
                     event.getMember().getUser().getAvatarUrl());
             event.getChannel().sendMessageEmbeds(embedBuilder.build()).queue();
